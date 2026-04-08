c907eed8ce2cfa7f8b1572360b1019c8ed1ad8f8
qbic <erichquerner@posteo.org>
2026-04-08 13:31:50 +0200

    implement conversion for text and bold

---
+++ src/test_conversion.py
@@ -0,0 +1,16 @@
+from textnode import TextNode, TextType
+from utils import text_node_to_html_node
+
+
+class TestConversion:
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        assert html_node.tag is None
+        assert html_node.value == "This is a text node"
+
+    def test_bold(self):
+        node = TextNode("This is bold text", TextType.BOLD)
+        html_node = text_node_to_html_node(node)
+        assert html_node.tag == "b"
+        assert html_node.value == "This is bold text"
---
+++ src/utils.py
@@ -0,0 +1,13 @@
+from htmlnode import HTMLNode
+from leafnode import LeafNode
+from textnode import TextNode, TextType
+
+
+def text_node_to_html_node(text_node: TextNode) -> HTMLNode:
+    match text_node.text_type:
+        case TextType.TEXT:
+            return LeafNode(tag=None, value=text_node.text)
+        case TextType.BOLD:
+            return LeafNode(tag="b", value=text_node.text)
+        case _:
+            raise ValueError("Unknown TextType.")
