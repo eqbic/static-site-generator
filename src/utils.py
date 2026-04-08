bfa75aba2e30770f2e69879da42b9a199d8bb1a4
qbic <erichquerner@posteo.org>
2026-04-08 15:35:47 +0200

    add copy/paste code for images and links

--- src/utils.py
+++ src/utils.py
@@ -17,12 +17,50 @@ def from_code(input: str) -> str:
     return re.sub(r"`(.*?)`", r"\1", input)
 
 
-def from_link(input: str) -> tuple[str, str]:
-    return re.findall(r"\[([^\]]+)\]\(([^)]+)\)", input)[0]
+def extract_markdown_links(text: str) -> list[tuple[str, str]]:
+    return re.findall(r"\[([^\]]+)\]\(([^)]+)\)", text)
 
 
-def from_image(input: str) -> tuple[str, str]:
-    return re.findall(r"!\[([^\]]*)\]\(([^)]+)\)", input)[0]
+def extract_markdown_images(text: str) -> list[tuple[str, str]]:
+    return re.findall(r"!\[([^\]]*)\]\(([^)]+)\)", text)
+    
+
+def split_nodes_link(old_nodes: list[TextNode]) -> list[TextNode]:
+    result = []
+    for old_node in old_nodes:
+        text = old_node.text
+        links = extract_markdown_links(text)
+        if len(links) == 0:
+            result.append(old_node)
+            continue
+        for link in links:
+            sections = text.split(f"[{link[0]}]({link[1]})", 1)
+            if len(sections[0]) > 0:
+                result.append(TextNode(sections[0], TextType.TEXT))
+            result.append(TextNode(link[0], TextType.LINK, link[1]))
+            text = sections[1]
+        if len(text) > 0:
+            result.append(TextNode(text, TextType.TEXT))
+    return result
+
+
+def split_nodes_image(old_nodes: list[TextNode]) -> list[TextNode]:
+    result = []
+    for old_node in old_nodes:
+        text = old_node.text
+        images = extract_markdown_images(text)
+        if len(images) == 0:
+            result.append(old_node)
+            continue
+        for image in images:
+            sections = text.split(f"![{image[0]}]({image[1]})", 1)
+            if len(sections[0]) > 0:
+                result.append(TextNode(sections[0], TextType.TEXT))
+            result.append(TextNode(image[0], TextType.IMAGE, image[1]))
+            text = sections[1]
+        if len(text) > 0:
+            result.append(TextNode(text, TextType.TEXT))
+    return result
 
 
 def split_nodes_delimiter(
@@ -32,6 +70,7 @@ def split_nodes_delimiter(
     for old_node in old_nodes:
         if old_node.text_type != TextType.TEXT:
             result.append(old_node)
+            continue
         parts = [
             TextNode(text, text_type if i % 2 == 1 else TextType.TEXT)
             for i, text in enumerate(old_node.text.split(delimiter))
