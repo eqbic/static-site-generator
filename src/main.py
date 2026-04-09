4ad144781c73163adab4a2758ef14473f6e8388c
qbic <erichquerner@posteo.org>
2026-04-09 08:44:59 +0200

    deploy from static to public

--- src/main.py
+++ src/main.py
@@ -1,16 +1,28 @@
+import shutil
+from pathlib import Path
+
 from htmlnode import HTMLNode
 from textnode import TextNode, TextType
 
+
+def find_project_root(start: Path, marker: str = "public") -> Path:
+    for parent in [start, *start.parents]:
+        if (parent / marker).exists():
+            return parent
+    raise RuntimeError("Project root not found")
+
+
+HERE = Path(__file__).resolve()
+PROJECT_ROOT = find_project_root(HERE)
+
+STATIC_DIR = PROJECT_ROOT / "static"
+PUBLIC_DIR = PROJECT_ROOT / "public"
+
+
+def deploy(source: Path, destination: Path):
+    shutil.rmtree(destination)
+    shutil.copytree(source, destination)
+
+
 if __name__ == "__main__":
-    text_node = TextNode("test", TextType.LINK, "https://boot.dev")
-    print(text_node)
-    html_node = HTMLNode(
-        "<p>",
-        "this is paragraph",
-        [],
-        {
-            "href": "https://www.google.com",
-            "target": "_blank",
-        },
-    )
-    print(html_node)
+    deploy(STATIC_DIR, PUBLIC_DIR)
