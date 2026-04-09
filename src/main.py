5b2739b96a57d6a8f72eacc974203b8fb2019384
qbic <erichquerner@posteo.org>
2026-04-09 10:03:09 +0200

    pass basepath

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+import sys
 from pathlib import Path
 from re import T
 
@@ -21,7 +22,9 @@ PUBLIC_DIR = PROJECT_ROOT / "public"
 CONTENT_DIR = PROJECT_ROOT / "content"
 
 
-def generate_page(from_path: Path, template_path: Path, dest_path: Path):
+def generate_page(
+    from_path: Path, template_path: Path, dest_path: Path, base_path: str
+):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     with open(from_path, "r") as f:
         md = f.read()
@@ -32,6 +35,8 @@ def generate_page(from_path: Path, template_path: Path, dest_path: Path):
     title = extract_title(md)
     template = template.replace("{{ Title }}", title)
     template = template.replace("{{ Content }}", html)
+    template = template.replace('href="/', f'href="{base_path}')
+    template = template.replace('src="/', f'src="{base_path}')
     rel_path = from_path.relative_to(CONTENT_DIR)
     dest_file = dest_path / rel_path.parent / f"{rel_path.stem}.html"
     if dest_file.exists():
@@ -42,11 +47,11 @@ def generate_page(from_path: Path, template_path: Path, dest_path: Path):
 
 
 def generate_pages_recursive(
-    content_path: Path, template_path: Path, dest_dir_path: Path
+    content_path: Path, template_path: Path, dest_dir_path: Path, base_path: str
 ):
     for root, dirs, files in os.walk(content_path):
         for file in files:
-            generate_page(Path(root) / file, template_path, dest_dir_path)
+            generate_page(Path(root) / file, template_path, dest_dir_path, base_path)
 
 
 def deploy_static(source: Path, destination: Path):
@@ -54,10 +59,13 @@ def deploy_static(source: Path, destination: Path):
     shutil.copytree(source, destination)
 
 
-def deploy_content(source: Path, destination: Path):
-    generate_pages_recursive(source, PROJECT_ROOT / "template.html", destination)
+def deploy_content(source: Path, destination: Path, base_path: str):
+    generate_pages_recursive(
+        source, PROJECT_ROOT / "template.html", destination, base_path
+    )
 
 
 if __name__ == "__main__":
+    base_path = sys.argv[0] if len(sys.argv) > 0 else "/"
     deploy_static(STATIC_DIR, PUBLIC_DIR)
-    deploy_content(CONTENT_DIR, PUBLIC_DIR)
+    deploy_content(CONTENT_DIR, PUBLIC_DIR, base_path)
